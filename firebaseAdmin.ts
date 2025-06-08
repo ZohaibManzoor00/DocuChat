2b508d49d081bda296c8d5559779b4891e35b8c5
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2025-06-08 19:25:39 -0400

    trial run for api keys

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,13 +1,24 @@
 import { initializeApp, getApps, cert, App, getApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceKey = require("./service_key.json");
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CERT_URL,
+};
 
-let app: App
+let app: App;
 
 if (getApps().length === 0) {
   app = initializeApp({
-    credential: cert(serviceKey),
+    credential: cert(serviceAccount as any),
   });
 } else {
   app = getApp();
